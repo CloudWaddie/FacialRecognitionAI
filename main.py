258072483d6f6a12e391db60a4f59c76bb933237
CloudWaddie <127060255+CloudWaddie@users.noreply.github.com>
2023-03-22 16:15:04 +1030

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from random import *
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-img = cv2.imread('face.jfif')
+image_path = input("What is the image filename?")
+
+img = cv2.imread(image_path)
 
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
